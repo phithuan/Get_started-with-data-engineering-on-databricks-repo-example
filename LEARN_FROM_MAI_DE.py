b58977587b87f9ed24bfb59bf648610dae797410
phithuan <phithuan>
2025-04-14 14:34:52 +0000

    nay học Cluster trong Databricks hinh đó là phân biệt

---
+++ LEARN_FROM_MAI_DE.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Cluster trong Databricks
+# MAGIC ![](/Volumes/workspace/ny_taxi/data_xe/Screenshot 2025-04-14 211535.png)
+# MAGIC
+
+# COMMAND ----------
+
+
